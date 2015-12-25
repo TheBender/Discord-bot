6c028721c97caf4bcc69b78d3c13c7ecd1055b43
TheBender <thebendertt@gmail.com>
2015-12-26 01:40:53 +0300

    топ горячего с пикабу совместил все говорилки переделал условие на find

--- hernya/VNIMANIE.py
+++ hernya/VNIMANIE.py
@@ -5,14 +5,12 @@ client.login('the_bender@mail.ru', 'Yfgjbkbvshe,kz')
 
 @client.event
 def on_message(message):
-    words = message.content.lower().split()
-    for word in words:
-        if word.startswith('внимание') and message.author.id != '129493465208848384':
-            quotes = message.content
-            writer = message.author.name
-            time = message.timestamp
-            client.send_message(message.channel, '**"**`' + quotes + '`**"**\n**' + writer + '** в ' + str(time.strftime("%H:%M")) + '\n\nСПАСИБО ЗА ВНИМАНИЕ!')
-            break
+    if message.content.lower().find('внимание'):
+        quotes = message.content
+        writer = message.author.name
+        time = message.timestamp
+        client.send_message(message.channel, '**"**`' + quotes + '`**"**\n**' + writer + '** в ' + str(time.strftime("%H:%M")) + '\n\nСПАСИБО ЗА ВНИМАНИЕ!')
+
 @client.event
 def on_ready():
     print('BOT INSTANCE IS RUNNING')
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import discord
+import re
+from rss_bot.pikabu import get_hot
+
 
 client = discord.Client()
 client.login('the_bender@mail.ru', 'Yfgjbkbvshe,kz')
 
 @client.event
 def on_message(message):
-    print(message.content)
-    print(message.author.name)
-    print(message.author.id)
-    if message.content.startswith('братишка'):
+
+    if message.content.startswith('!get-pikabu-hot') and message.author.id != '129493465208848384':
+        num = re.search('!get-pikabu-hot (\d+)', message.content)
+        print(num.group(1))
+        top = get_hot(num.group(1))
+        for post in top:
+            client.send_message(message.channel, "**[" + post['rating'] + "]** **`" + post['header'] + "`**\n __" + post['creation_time'] + "__\n```" + post['link'] + "```\n\n")
+
+    if message.content.lower().find('внимание') != -1 and message.author.id != '129493465208848384':
+        quotes = message.content
+        writer = message.author.name
+        time = message.timestamp
+        client.send_message(message.channel, '**"**`' + quotes + '`**"**\n**' + writer + '** в ' + str(time.strftime("%H:%M")) + '\n\nСПАСИБО ЗА ВНИМАНИЕ!')
+
+    if message.content.startswith('братишка') and message.author.id != '129493465208848384':
         client.send_message(message.channel, 'Я вам покушать принес!')
-    if (message.author.id == '128768252921774080' or message.author.id == '128780999134806016' or message.author.id == '128775115075747840') and message.content.startswith('ублюдок'):
-        client.send_message(message.channel, 'Покидьок, твою мать, ану йди сюди, лайно собаче, вирішив до мене лізти? Ти, засранець смердючий, матір твою, га? Ну йди сюди, спробуй мене трахнути, я тебе сам трахну покидьок, онаніст дідьків, будь ти проклятий, йди ідіот, трахати тебе й усю твою родину, лайно собаче, жлоб смердючий, лайно, сука, падла, йди сюди, мерзотник, негідник, гад, йди сюди ти - лайно, ДУПА!')
+
+    if message.content.lower().find('ублюдок') != -1 and message.author.id != '129493465208848384':
+        client.send_message(message.channel, 'Покидьок, твою мать, ану йди сюди, лайно собаче, вирішив до мене лізти? Ти,'
+                                             ' засранець смердючий, матір твою, га? Ну йди сюди, спробуй мене трахнути, '
+                                             'я тебе сам трахну покидьок, онаніст дідьків, будь ти проклятий, йди ідіот, '
+                                             'трахати тебе й усю твою родину, лайно собаче, жлоб смердючий, лайно, сука, '
+                                             'падла, йди сюди, мерзотник, негідник, гад, йди сюди ти - лайно, ДУПА!')
 
 @client.event
 def on_ready():
